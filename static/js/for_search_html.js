141969a4c80e9a2073a1d7dce3e4f3e65afeee47
noelvalent <nv.noelvalent@gmail.com>
2020-04-21 00:20:05 +0900

    [ADD] 검색기능 추가, 검색 쿼리에 맞는 페이지 이동 기능 추가

--- static/js/for_search_html.js
+++ static/js/for_search_html.js
@@ -10,13 +10,35 @@ $(function(){
     `);
 });
 
+function get_params_parser() {
+    let params = location.search.substr(location.search.indexOf("?")+1).split("&");
+    let mappedParams = {};
+    let temp;
+    for (let index in params) {
+        temp = params[index].split("=");
+        mappedParams[temp[0]] = temp[1];
+    }
+    return mappedParams;
+}
+
+function go_to_page(pageNumber) {
+    let getParamsObject = get_params_parser();
+    let href;
+    if (getParamsObject["category"] !== undefined) {
+        href = `?category=${getParamsObject["category"]}&query=${getParamsObject["query"]}&page=${pageNumber}`;
+    } else {
+        href = `?page=${pageNumber}`;
+    }
+    location.href = href;
+}
+
 function go_to_page_using_select() {
     let pageNumberSelect = document.getElementById("pageNumberSelect");
     let selectPageNumber = pageNumberSelect.options[pageNumberSelect.selectedIndex].value;
-    location.href = `?page=${selectPageNumber}`;
+    go_to_page(selectPageNumber);
 }
 
 function go_to_page_using_input() {
     let pageNumberInput = document.getElementById("pageNumberInput");
-    location.href = `?page=${pageNumberInput.value}`;
+    go_to_page(pageNumberInput.value);
 }
